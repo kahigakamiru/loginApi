7cf7ae8768413dcd1c685ab22cab884163050a5f
kahiga <kahigakamiru@gmail.com>
2021-11-08 11:18:50 +0300

    adjusting password

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pool = createPool({
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
     connectionLimit: 10
 });
